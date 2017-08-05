b984adfbc13c176e3348d51a1da9fe123781675a
computingattendence <computingattendence@gmail.com>
2017-08-05 23:14:24 +0530

    Token Detect

--- src/Modules/LexicalParser.java
+++ src/Modules/LexicalParser.java
@@ -3,7 +3,6 @@ package Modules;
 import Models.LexicalUnit;
 import Models.TokenSets;
 import java.util.ArrayList;
-import java.util.Stack;
 
 public class LexicalParser {
 
@@ -16,16 +15,44 @@ public class LexicalParser {
     public boolean ValidateSyntax() {
 
         if (Tokens.get(0).getToken() == TokenSets.Begin) {
-            
-            if (Tokens.get(Tokens.size() - 1).getToken() == TokenSets.EndB) {
 
-                for(int i=1;i<Tokens.size()-1;i++)
-                {
-                    
+            if (Tokens.get(Tokens.size() - 1).getToken() == TokenSets.EndCB) {
+
+                for (int i = 1; i < Tokens.size() - 1; i++) {
+
+                    if (Tokens.get(i).getToken() == TokenSets.DataType) {
+                        if (Tokens.get(i + 4).getToken() == TokenSets.SemiC) {
+
+                            if (Tokens.get(i + 2).getToken() != TokenSets.Equal) {
+
+                                System.out.println("Assignment Operator Missing Near" + Tokens.get(i).getUnitName());
+                            } else if (Tokens.get(i + 3).getToken() != TokenSets.Values) {
+                                if (Tokens.get(i + 3).getToken() == TokenSets.Identifier) {
+                                    boolean validin = false;
+                                    for (int j = 0; j < (i + 3); j++) {
+                                        if (Tokens.get(j).getUnitName().equals(Tokens.get(i + 3).getUnitName())) {
+                                            validin = true;
+                                        }
+                                    }
+                                    if (!validin) {
+                                        System.out.println("Error not initialized");
+                                    }
+                                } else {
+
+                                }
+                            }
+                        } else {
+                            System.out.println("Missing SemiColon Near " + Tokens.get(i).getUnitName());
+                        }
+
+                    } else {
+                      
+                    }
+
                 }
                 return true;
             } else {
-                System.out.println("The Program Should end with Start");
+                System.out.println("The Program Should end with an }");
                 return false;
             }
         } else {

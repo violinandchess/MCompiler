acc8cc1e20f32fdc64ce3cf89b1d7511a31e55e5
computingattendence <computingattendence@gmail.com>
2017-08-05 22:27:23 +0530

    Token Detect

--- src/Modules/LexicalParser.java
+++ src/Modules/LexicalParser.java
@@ -1,4 +1,38 @@
 package Modules;
 
+import Models.LexicalUnit;
+import Models.TokenSets;
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class LexicalParser {
+
+    ArrayList<LexicalUnit> Tokens;
+
+    public LexicalParser(ArrayList<LexicalUnit> Tokens) {
+        this.Tokens = Tokens;
+    }
+
+    public boolean ValidateSyntax() {
+
+        if (Tokens.get(0).getToken() == TokenSets.Begin) {
+            
+            if (Tokens.get(Tokens.size() - 1).getToken() == TokenSets.EndB) {
+
+                for(int i=1;i<Tokens.size()-1;i++)
+                {
+                    
+                }
+                return true;
+            } else {
+                System.out.println("The Program Should end with Start");
+                return false;
+            }
+        } else {
+            System.out.println("The Program Should begin with Start");
+            return false;
+        }
+
+    }
+
 }
